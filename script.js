e0411657f7ec50b091ae0662e04f1667dd5675a4
josealdunate <aldunate.jose91@gmail.com>
2021-03-23 20:02:43 -0300

    updated hero and call to action background-images borders; added JS to toggle navbar background

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-let header = document.getElementsByClassName('page-header');
+const pageHeader = document.querySelector(".page-header");
+const headerLogo = document.querySelector(".logo-div--small__hide");
+const sectionHero = document.querySelector("#hero");
 
-window.addEventListener('scroll', function() {
-	if (document.body.scrollTop > 20) {
-        header.style.border = "1px solid red";
-    }
-	
-});
+const sectionHeroOptions = {
+    rootMargin: "-300px 0px 0px 0px"
+};
+
+const sectionHeroObserver = new IntersectionObserver
+(function(
+    entries, 
+    sectionHeroObserver
+) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            pageHeader.classList.add("page-header--background");
+            headerLogo.classList.add("logo-div--small__show");
+        } else {
+            pageHeader.classList.remove("page-header--background");
+            headerLogo.classList.remove("logo-div--small__show");
+        }
+    });
+}, 
+sectionHeroOptions);
+
+sectionHeroObserver.observe(sectionHero);
